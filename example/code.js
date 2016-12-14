01705ebd96bfb60bc96a4e93854f345b238d18b1
I'amee <admin@cavacn.com>
2016-12-14 15:11:38 +0800

    增加 and or 函数

--- example/code.js
+++ example/code.js
@@ -18,9 +18,11 @@
 // let Ma60 = EMA(CLOSE, 60);
 // log.info(yield COUNT(GT(CLOSE,OPEN),20));
 // log.info(yield COUNT(GT(CLOSE(),OPEN()),20));
-log.info(yield BARSLAST(GT(DIV(CLOSE(),REF(CLOSE(),1)),1.1)));
+// log.info(yield BARSLAST(GT(DIV(CLOSE(),REF(CLOSE(),1)),1.1)));
 // log.info(yield ABS(GT(CLOSE,OPEN),20));
 // let AA = BARSLAST(yield Ma40 > yield Ma60 && ( yield Ma20 = Ma40 || Ma20 > Ma40));
 // let BB = REF(COUNT(yield CLOSE < yield Ma60, yield AA) = AA, 1);
 // let CC = CROSS(CLOSE,Ma60) && VOLUME > yield MA(VOLUME,5) && (CLOSE-REF(CLOSE,1))/REF(CLOSE,1)*100 > 3;
 // let FRCS: yield BB && yield CC;
+
+log.info(yield AND(GT(CLOSE(),OPEN()),GT(VOLUME(),REF(VOLUME(),1))));
