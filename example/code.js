4de174c4bf5f0c0022804bba9bb6b3fce563a249
I'amee <admin@cavacn.com>
2016-12-16 11:31:21 +0800

    内置KEY，更新为括号分割

--- example/code.js
+++ example/code.js
@@ -11,21 +11,9 @@
 // CC:=CROSS(CLOSE,MA60) AND VOL > MA(VOL,5) and (CLOSE-REF(CLOSE,1))/REF(CLOSE,1)*100>3;
 // FRCS:BB AND CC;
 
-// log.info(yield SLOPE(HHV(CLOSE,5),5));
 
-let Ma20 = EMA(CLOSE, 5);
-// log.info(CLOSE.DIV(OPEN));
-// log.info(yield COUNT(GT(CLOSE(),OPEN()),20));
-// log.info(yield BARSLAST(GT(DIV(CLOSE(),REF(CLOSE(),1)),1.1)));
-// log.info(yield Ma20,yield Ma40,yield Ma60);
-// log.info(yield ABS(GT(CLOSE,OPEN),20));
-log.info(Ma20)
-// let BB = REF(EQ(COUNT(LT(CLOSE(), Ma60),AA)), 1);
-// let CC = CROSS(CLOSE(), Ma60)
-// log.info(yield TIME());
-// let BB = REF(COUNT(yield CLOSE < yield Ma60, yield AA) = AA, 1);
-// let CC = CROSS(CLOSE,Ma60) && VOLUME > yield MA(VOLUME,5) && (CLOSE-REF(CLOSE,1))/REF(CLOSE,1)*100 > 3;
-// let FRCS: yield BB && yield CC;
-
-// log.info(yield OR(GT(CLOSE(),OPEN()),GT(VOLUME(),REF(VOLUME(),1))));
-
+let MA20 = EMA(CLOSE, 20);
+let MA40 = EMA(CLOSE, 40);
+let MA60 = EMA(CLOSE, 60);
+let AA = BARSLAST(MA40.GT(MA60).AND(MA20.EQ(MA40).OR(MA20.GT(MA40))));
+log.info(COUNT(CLOSE.LT(MA60), AA).EQ(AA))
--- example/demo.js
+++ example/demo.js
@@ -5,22 +5,33 @@ const Strategy = require('../lib/strategy');
 const path = require('path');
 const fetch = require('node-fetch');
 let demo = new Strategy('demo', path.join(__dirname, './code.js'));
-//
-// demo.onTick({open: 10, close: 11, low: 9, high: 12, volume: 100, turnover: 1000})
-// demo.onTick({open: 10, close: 10, low: 9, high: 12, volume: 100, turnover: 1000})
-// demo.onTick({open: 10, close: 12, low: 9, high: 12, volume: 100, turnover: 1000})
-// demo.onTick({open: 10, close: 13, low: 9, high: 12, volume: 100, turnover: 1000})
-// demo.onTick({open: 10, close: 9, low: 9, high: 12, volume: 100, turnover: 1000})
-// demo.onTick({open: 10, close: 6, low: 9, high: 12, volume: 100, turnover: 1000})
-// demo.onTick({open: 10, close: 23, low: 9, high: 12, volume: 100, turnover: 1000})
-// demo.onTick({open: 10, close: 14, low: 9, high: 12, volume: 100, turnover: 1000})
-// demo.onTick({open: 10, close: 22, low: 9, high: 12, volume: 100, turnover: 1000})
-// demo.onTick({open: 10, close: 9, low: 9, high: 12, volume: 100, turnover: 1000})
+// const Redis = require('redis');
+// const local = Redis.createClient();
+// local.zrange('SS.DAYS.600233.DATE.SSE.A_STOCK', 0, -1, function (err, result) {
+//     console.log(err, result.length)
+//     result.map((item) => {
+//         let items = item.split(',');
+//         return {
+//             time: parseFloat(items[0]),
+//             open: parseFloat(items[1]),
+//             low: parseFloat(items[2]),
+//             high: parseFloat(items[3]),
+//             close: parseFloat(items[4]),
+//             volume: parseFloat(items[5])
+//         }
+//     }).forEach((item) => {
+//         // console.log(new Date(item.time),item)
+//         demo.onTick(item);
+//     })
+// })
+
 let url = `https://api.cavacn.com/tools/stock/quotation/0.2/days/SH/600233`
 fetch(url).then((res)=>{return res.json()}).then((json)=>{
     json.result.forEach(function(item){
         demo.onTick(item)
     })
     console.log("==>",json.result.length);
-
+    demo.context.datas.forEach(function(item){
+        console.log(item)
+    })
 })
