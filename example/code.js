a1578d1b2e62498c772b55d57b963d76d8bc9fac
I'amee <admin@cavacn.com>
2016-12-16 15:03:58 +0800

    增加可控参数fixed

--- example/code.js
+++ example/code.js
@@ -18,7 +18,7 @@
 let N = 9;
 let M1 = 3;
 let M2 = 3;
-let RSV = CLOSE.SUB(LLV(LOW, N)).DIV(HHV(HIGH, N).SUB(LLV(LOW, N).MUL(100)));
+let RSV = CLOSE.SUB(LLV(LOW, N)).DIV(HHV(HIGH, N).SUB(LLV(LOW, N))).MUL(100);
 let K = SMA(RSV, M1, 1);
 let D = SMA(K, M2, 1);
 let J = K.MUL(3).SUB(D.MUL(2))
