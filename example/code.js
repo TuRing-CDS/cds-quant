411a228974827b0ba05b583e611474accd4e5347
I'amee <admin@cavacn.com>
2016-12-27 15:58:10 +0800

    修正KDJ指数计算错误，主要原因hhv，let 两次

--- example/code.js
+++ example/code.js
@@ -13,23 +13,24 @@
 // draw.stick('MACD', MACD, {bear: {color: 'green'}, bull: {color: 'red'}, screen: 'sc1'});
 // draw.line('DEA', DEA, {color: '#00ffff', screen: 'sc1'});
 //
-// // KDJ
-// let N = 9;
-// let M1 = 3;
-// let M2 = 3;
-// let RSV = CLOSE.SUB(LLV(LOW, N)).DIV(HHV(HIGH, N).SUB(LLV(LOW, N))).MUL(100);
-// let K = SMA(RSV, M1, 1);
-// let D = SMA(K, M2, 1);
-// let J = MUL(K, 3).SUB(MUL(D, 2));
-// draw.line('K', K, {color: '#ff00ff', screen: 'sc2'});
-// draw.line('D', D, {color: '#ff66ff', screen: 'sc2'});
-// draw.line('J', J, {color: '#ff99ff', screen: 'sc2'});
-// // stock.choose(K);
-// log.info(K,D,J)
 
 // CCI
-let N2 = 14;
-let TYP = DIV(ADD(HIGH, ADD(LOW, CLOSE)), 3);
-let CCI = DIV(SUB(TYP, MA(TYP, N2)), MUL(0.015, AVERAGE(TYP, N2)));
-draw.line('CCI', CCI, {color: '#ff0000', screen: 'sc3'});
-log.info(CCI)
+// let N2 = 14;
+// let TYP = DIV(ADD(HIGH, ADD(LOW, CLOSE)), 3);
+// let CCI = DIV(SUB(TYP, MA(TYP, N2)), MUL(0.015, AVERAGE(TYP, N2)));
+// draw.line('CCI', CCI, {color: '#ff0000', screen: 'sc3'});
+// log.info(CCI)
+
+// // KDJ
+let N = 9;
+let M1 = 3;
+let M2 = 3;
+let RSV = CLOSE.SUB(LLV(LOW, N)).DIV(HHV(HIGH, N).SUB(LLV(LOW, N))).MUL(100);
+let K = SMA(RSV, M1, 1);
+let D = SMA(K, M2, 1);
+let J = MUL(K, 3).SUB(MUL(D, 2));
+draw.line('K', K, {color: '#ff00ff', screen: 'sc2'});
+draw.line('D', D, {color: '#ff66ff', screen: 'sc2'});
+draw.line('J', J, {color: '#ff99ff', screen: 'sc2'});
+// stock.choose(K);
+log.info(K,D,J)
