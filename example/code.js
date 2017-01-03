d14843689e789b5faf9e2aad34c341d3941f1471
I'amee <admin@cavacn.com>
2017-01-03 15:39:43 +0800

    增加IF函数 修正getValue函数内部实现

--- example/code.js
+++ example/code.js
@@ -15,17 +15,16 @@
 //
 
 // // KDJ
-// let N = 9;
-// let M1 = 3;
-// let M2 = 3;
-// let RSV = CLOSE.SUB(LLV(LOW, N)).DIV(HHV(HIGH, N).SUB(LLV(LOW, N))).MUL(100);
-// let K = SMA(RSV, M1, 1);
-// let D = SMA(K, M2, 1);
-// let J = MUL(K, 3).SUB(MUL(D, 2));
+let N = 9;
+let M1 = 3;
+let M2 = 3;
+let RSV = CLOSE.SUB(LLV(LOW, N)).DIV(HHV(HIGH, N).SUB(LLV(LOW, N))).MUL(100);
+let K = SMA(RSV, M1, 1);
+let D = SMA(K, M2, 1);
+let J = MUL(K, 3).SUB(MUL(D, 2));
 // draw.line('K', K, {color: '#ff00ff', screen: 'sc2'});
 // draw.line('D', D, {color: '#ff66ff', screen: 'sc2'});
 // draw.line('J', J, {color: '#ff99ff', screen: 'sc2'});
-// // log.info(K,D,J)
 // score.choose(K);
 //
 // // CCI
@@ -35,19 +34,41 @@
 // draw.line('CCI', CCI, {color: '#ff0000', screen: 'sc3'});
 // log.info(CCI)
 
-//RSI
-
+// RSI
+//
 // REFLINE: 0, 20, 50, 80, 100;
 // LC := REF(CLOSE,1);
 // RSI1:SMA(MAX(CLOSE-LC,0),N1,1)/SMA(ABS(CLOSE-LC),N1,1)*100;
 // RSI2:SMA(MAX(CLOSE-LC,0),N2,1)/SMA(ABS(CLOSE-LC),N2,1)*100;
 // RSI3:SMA(MAX(CLOSE-LC,0),N3,1)/SMA(ABS(CLOSE-LC),N3,1)*100;
 
-let RSIN1 = 6;
-let RSIN2 = 12;
-let RSIN3 = 24;
-let LC = REF(CLOSE, 1);
-let RSI1 = SMA(MAX(CLOSE.SUB(LC), 0), RSIN1, 1).DIV(SMA(ABS(CLOSE.SUB(LC)), RSIN1, 1)).MUL(100);
-let RSI2 = SMA(MAX(CLOSE.SUB(LC), 0), RSIN2, 1).DIV(SMA(ABS(CLOSE.SUB(LC)), RSIN2, 1)).MUL(100);
-let RSI3 = SMA(MAX(CLOSE.SUB(LC), 0), RSIN3, 1).DIV(SMA(ABS(CLOSE.SUB(LC)), RSIN3, 1)).MUL(100);
-log.info(RSI1,RSI2,RSI3)
+// let RSIN1 = 6;
+// let RSIN2 = 12;
+// let RSIN3 = 24;
+// let LC = REF(CLOSE, 1);
+// let RSI1 = SMA(MAX(CLOSE.SUB(LC), 0), RSIN1, 1).DIV(SMA(ABS(CLOSE.SUB(LC)), RSIN1, 1)).MUL(100);
+// let RSI2 = SMA(MAX(CLOSE.SUB(LC), 0), RSIN2, 1).DIV(SMA(ABS(CLOSE.SUB(LC)), RSIN2, 1)).MUL(100);
+// let RSI3 = SMA(MAX(CLOSE.SUB(LC), 0), RSIN3, 1).DIV(SMA(ABS(CLOSE.SUB(LC)), RSIN3, 1)).MUL(100);
+// log.info(RSI1,RSI2,RSI3)
+
+//DMI
+
+// TR := SUM(MAX(MAX(HIGH-LOW,ABS(HIGH-REF(CLOSE,1))),ABS(LOW-REF(CLOSE,1))),N);
+// HD := HIGH-REF(HIGH,1);
+// LD := REF(LOW,1)-LOW;
+// DMP:= SUM(IF(HD>0 AND HD>LD,HD,0),N);
+// DMM:= SUM(IF(LD>0 AND LD>HD,LD,0),N);
+// PDI: DMP*100/TR;
+// MDI: DMM*100/TR;
+// ADX: MA(ABS(MDI-PDI)/(MDI+PDI)*100,M);
+// ADXR:(ADX+REF(ADX,M))/2;
+
+// let N = 14;
+// let M = 6;
+
+// let TR = SUM(Max(MAX(HIGH.SUB(LOW), ABS(HIGH.SUB(REF(CLOSE, 1)))), ABS(LOW.SUB(REF(CLOSE, 1)))), N);
+// let HD = HIGH.SUB(REF(HIGH,1));
+// let LD = REF(LOW,1).SUB(LOW);
+// let DEMOIF = IF(CLOSE.GT(REF(CLOSE,1)),'true---','false===');
+// log.info(DEMOIF);
+log.info(K,D,J);
