d0ad4fbefae0d987fb4ec0bcb07d1e725fb5d0a5
I'amee <admin@cavacn.com>
2017-02-08 11:22:26 +0800

    优化abs,ma。 清除重复代码

--- example/code.js
+++ example/code.js
@@ -1,6 +1,6 @@
-/**
- * Created by Z on 2016-12-13.
- */
+// /**
+//  * Created by Z on 2016-12-13.
+//  */
 // // MACD
 // let SHORT = 12;
 // let LONG = 26;
@@ -13,8 +13,9 @@
 // draw.stick('MACD', MACD, {bear: {color: 'green'}, bull: {color: 'red'}, screen: 'sc1'});
 // draw.line('DEA', DEA, {color: '#00ffff', screen: 'sc1'});
 //
-
+//
 // // KDJ
+//
 // let N = 9;
 // let M1 = 3;
 // let M2 = 3;
@@ -25,55 +26,60 @@
 // draw.line('K', K, {color: '#ff00ff', screen: 'sc2'});
 // draw.line('D', D, {color: '#ff66ff', screen: 'sc2'});
 // draw.line('J', J, {color: '#ff99ff', screen: 'sc2'});
-// score.choose(K);
+// // score.choose(K);
+// //
+//
+// // log.info(CCI)
+//
+// // RSI
+// //
+// // REFLINE: 0, 20, 50, 80, 100;
+// // LC := REF(CLOSE,1);
+// // RSI1:SMA(MAX(CLOSE-LC,0),N1,1)/SMA(ABS(CLOSE-LC),N1,1)*100;
+// // RSI2:SMA(MAX(CLOSE-LC,0),N2,1)/SMA(ABS(CLOSE-LC),N2,1)*100;
+// // RSI3:SMA(MAX(CLOSE-LC,0),N3,1)/SMA(ABS(CLOSE-LC),N3,1)*100;
+//
+// // let RSIN1 = 6;
+// // let RSIN2 = 12;
+// // let RSIN3 = 24;
+// // let LC = REF(CLOSE, 1);
+// // let RSI1 = SMA(MAX(CLOSE.SUB(LC), 0), RSIN1, 1).DIV(SMA(ABS(CLOSE.SUB(LC)), RSIN1, 1)).MUL(100);
+// // let RSI2 = SMA(MAX(CLOSE.SUB(LC), 0), RSIN2, 1).DIV(SMA(ABS(CLOSE.SUB(LC)), RSIN2, 1)).MUL(100);
+// // let RSI3 = SMA(MAX(CLOSE.SUB(LC), 0), RSIN3, 1).DIV(SMA(ABS(CLOSE.SUB(LC)), RSIN3, 1)).MUL(100);
+// // log.info(RSI1,RSI2,RSI3)
+//
+// //DMI
+//
+// // TR := SUM(MAX(MAX(HIGH-LOW,ABS(HIGH-REF(CLOSE,1))),ABS(LOW-REF(CLOSE,1))),N);
+// // HD := HIGH-REF(HIGH,1);
+// // LD := REF(LOW,1)-LOW;
+// // DMP:= SUM(IF(HD>0 AND HD>LD,HD,0),N);
+// // DMM:= SUM(IF(LD>0 AND LD>HD,LD,0),N);
+// // PDI: DMP*100/TR;
+// // MDI: DMM*100/TR;
+// // ADX: MA(ABS(MDI-PDI)/(MDI+PDI)*100,M);
+// // ADXR:(ADX+REF(ADX,M))/2;
+//
+// // log.info(K,D,J);
+// // let N = 14;
+// // let M = 6;
+// //
+// // let TR = SUM(MAX(MAX(HIGH.SUB(LOW), ABS(HIGH.SUB(REF(CLOSE, 1)))), ABS(LOW.SUB(REF(CLOSE, 1)))), N);
+// // let HD = HIGH.SUB(REF(HIGH, 1));
+// // let LD = REF(LOW, 1).SUB(LOW);
+// // let DMP = SUM(IF(HD.GT(0).AND(HD.GT(LD)), HD, 0), N);
+// // let DMM = SUM(IF(LD.GT(0).AND(LD.GT(HD)), LD, 0), N);
+// // let PDI = DMP.MUL(100).DIV(TR);
+// // let MDI = DMM.MUL(100).DIV(TR);
+// // let ADX = MA(ABS(MDI.SUB(PDI)).DIV(MDI.ADD(PDI)).MUL(100), M);
+// // let ADXR = ADX.ADD(REF(ADX, M)).DIV(2);
+// // log.info(PDI, MDI, ADX, ADXR);
 //
 // // CCI
 // let N2 = 14;
 // let TYP = DIV(ADD(HIGH, ADD(LOW, CLOSE)), 3);
 // let CCI = DIV(SUB(TYP, MA(TYP, N2)), MUL(0.015, AVERAGE(TYP, N2)));
 // draw.line('CCI', CCI, {color: '#ff0000', screen: 'sc3'});
-// log.info(CCI)
 
-// RSI
-//
-// REFLINE: 0, 20, 50, 80, 100;
-// LC := REF(CLOSE,1);
-// RSI1:SMA(MAX(CLOSE-LC,0),N1,1)/SMA(ABS(CLOSE-LC),N1,1)*100;
-// RSI2:SMA(MAX(CLOSE-LC,0),N2,1)/SMA(ABS(CLOSE-LC),N2,1)*100;
-// RSI3:SMA(MAX(CLOSE-LC,0),N3,1)/SMA(ABS(CLOSE-LC),N3,1)*100;
-
-// let RSIN1 = 6;
-// let RSIN2 = 12;
-// let RSIN3 = 24;
-// let LC = REF(CLOSE, 1);
-// let RSI1 = SMA(MAX(CLOSE.SUB(LC), 0), RSIN1, 1).DIV(SMA(ABS(CLOSE.SUB(LC)), RSIN1, 1)).MUL(100);
-// let RSI2 = SMA(MAX(CLOSE.SUB(LC), 0), RSIN2, 1).DIV(SMA(ABS(CLOSE.SUB(LC)), RSIN2, 1)).MUL(100);
-// let RSI3 = SMA(MAX(CLOSE.SUB(LC), 0), RSIN3, 1).DIV(SMA(ABS(CLOSE.SUB(LC)), RSIN3, 1)).MUL(100);
-// log.info(RSI1,RSI2,RSI3)
-
-//DMI
-
-// TR := SUM(MAX(MAX(HIGH-LOW,ABS(HIGH-REF(CLOSE,1))),ABS(LOW-REF(CLOSE,1))),N);
-// HD := HIGH-REF(HIGH,1);
-// LD := REF(LOW,1)-LOW;
-// DMP:= SUM(IF(HD>0 AND HD>LD,HD,0),N);
-// DMM:= SUM(IF(LD>0 AND LD>HD,LD,0),N);
-// PDI: DMP*100/TR;
-// MDI: DMM*100/TR;
-// ADX: MA(ABS(MDI-PDI)/(MDI+PDI)*100,M);
-// ADXR:(ADX+REF(ADX,M))/2;
-
-// log.info(K,D,J);
-// let N = 14;
-// let M = 6;
-//
-// let TR = SUM(MAX(MAX(HIGH.SUB(LOW), ABS(HIGH.SUB(REF(CLOSE, 1)))), ABS(LOW.SUB(REF(CLOSE, 1)))), N);
-// let HD = HIGH.SUB(REF(HIGH, 1));
-// let LD = REF(LOW, 1).SUB(LOW);
-// let DMP = SUM(IF(HD.GT(0).AND(HD.GT(LD)), HD, 0), N);
-// let DMM = SUM(IF(LD.GT(0).AND(LD.GT(HD)), LD, 0), N);
-// let PDI = DMP.MUL(100).DIV(TR);
-// let MDI = DMM.MUL(100).DIV(TR);
-// let ADX = MA(ABS(MDI.SUB(PDI)).DIV(MDI.ADD(PDI)).MUL(100), M);
-// let ADXR = ADX.ADD(REF(ADX, M)).DIV(2);
-log.info(PDI, MDI, ADX, ADXR);
+log.info(MA(ABS(CLOSE),5));
+log.info(ABS(CLOSE));
