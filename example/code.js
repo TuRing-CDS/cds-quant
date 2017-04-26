4bbc5bc0e391d94c25ae4f84538f7bbd8969e339
I'amee <admin@cavacn.com>
2017-04-26 15:15:40 +0800

    增加or函数

--- example/code.js
+++ example/code.js
@@ -89,6 +89,6 @@
 // let RSI3 = SMA(MAX(CLOSE.SUB(LC), 0), RSIN3, 1).DIV(SMA(ABS(CLOSE.SUB(LC)), RSIN3, 1)).MUL(100);
 // IF(RSI1.LT(RSI2), BUY(1), SHORT(1))();
 
-let a = MEMA(C,2);
+let a = OR(GT(CLOSE,OPEN),GT(CLOSE,OPEN));
 
 log.info(a);
