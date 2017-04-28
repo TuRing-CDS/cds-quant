ba2ba5b587c6683a03a1b29d3691890be04fd63b
I'amee <admin@cavacn.com>
2017-04-28 11:48:53 +0800

    增加score 函数

--- example/demo.js
+++ example/demo.js
@@ -7,18 +7,21 @@ const fs = require('fs');
 
 const fetch = require('node-fetch');
 
-const demo = new Strategy('demo', fs.readFileSync('./code.js').toString(), {b: 30});
+// const opts = {cache: {maxLength: 10}};
+const opts = {};
 
-// console.log('===>语法错误', demo.check());
+const demo = new Strategy('demo', fs.readFileSync('./code.js').toString(), opts, {b: 30});
+
+console.log('===>语法错误', demo.check());
 
 
-let url = `http://api.cavacn.com:8888/tools/stock/quotation/0.2/days/SH/600233`;
-
-fetch(url).then((res) => {
-    return res.json();
-}).then((json) => {
-    json.result.forEach((item) => {
-        demo.onBar(item);
-        console.log('===>',demo.getScore());
-    })
-});
+// let url = `http://api.cavacn.com:8888/tools/stock/quotation/0.2/days/SH/600233`;
+//
+// fetch(url).then((res) => {
+//     return res.json();
+// }).then((json) => {
+//     json.result.forEach((item) => {
+//         demo.onBar(item);
+//         console.log('===>', demo.getScore());
+//     })
+// });
